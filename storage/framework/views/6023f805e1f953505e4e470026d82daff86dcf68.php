175a208d16e3ab9e3b8a74a619e72d9defac986e
Khaled Abdallah <khaledabdalah488@gmail.com>
2025-12-22 15:33:42 +0200

    clear all compiled pages

--- storage/framework/views/6023f805e1f953505e4e470026d82daff86dcf68.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2024 <a href="">Dar Elnasher</a>.</strong> All rights reserved.
-  </footer>
-<?php /**PATH C:\xampp\htdocs\nasher\resources\views/admin/includes/footer.blade.php ENDPATH**/ ?>
